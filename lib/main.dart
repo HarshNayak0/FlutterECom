9a65e645b6d117f45b6bf6fd56a0a4d432221321
HarshNayak <harshnayak@HarshNayaks-MacBook-Pro.local>
2023-09-05 16:29:25 -0400

    Ecommerce Website on Flutter

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+      elevation: 4,
+      title: Text("Articulate",
+          style: TextStyle(color: Theme.of(context).colorScheme.onPrimary)),
+      backgroundColor: Theme.of(context).colorScheme.primary,
+    ));
+  }
+}
